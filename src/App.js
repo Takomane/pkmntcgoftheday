030b1a59f4a576ece4507deeebc7b5a63c6daeba
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2022-06-16 09:32:21 -0400

    added card find to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import pokemon from 'pokemontcgsdk';
+import card from 'pokemontcgsdk/es/queryBuilder';
+pokemon.configure({apiKey: '6ba0faa6-c888-4290-ab79-7c1813b1c68e'});
 
 function App() {
   return (
@@ -7,6 +10,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+pokemon.card.find('base1-4')
+.then(card = {
+console.log(card.name)
+})
     </div>
   );
 }
